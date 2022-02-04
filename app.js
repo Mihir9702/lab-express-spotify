866d211a33904714b7f80f7ccc4ee6ad450ff337
Mihir Patel <mihirkpatel9702@gmail.com>
2022-02-04 15:38:04 -0500

    done, can work on styles better

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 
 const express = require('express');
+const res = require('express/lib/response');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -12,7 +13,43 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+})
+
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(err => console.error(err))
+
 
 // Our routes go here:
+// Home Page
+app.get('/', (req, res) => res.render('index'))
+
+// Search Artist
+app.get('/artist-search', (req, res) => {
+    spotifyApi
+        .searchArtists(req.query.artist)  
+        .then(data => res.render('artists', { artists: data.body.artists.items }))
+        .catch(err => console.error(err))
+})
+
+// Albums
+app.get('/albums/:id', (req, res) => {
+    spotifyApi
+        .getArtistAlbums(req.params.id)
+        .then(data => res.render('albums', { albums: data.body.items }))
+        .catch(err => console.error(err))
+})
+
+// Tracks
+app.get('/tracks/:id', (req, res) => {
+    spotifyApi
+        .getAlbumTracks(req.params.id)
+        .then(data => res.render('tracks', { tracks: data.body.items }))
+        .catch(err => console.error(err))
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
